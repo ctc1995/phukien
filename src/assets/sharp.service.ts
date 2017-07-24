a15edb27dfc9ab6513d2597e26c9e05858a6647f
Benson.Cai <495799915@qq.com>
2017-07-24 15:19:42 +0800

    nothing

--- src/assets/sharp.service.ts
+++ src/assets/sharp.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SharpService {
     private httpHader = "http://"
-    private address = "192.168.30.104:3000/";//10.28.83.150:3000/
+    private address = "192.168.30.105:3000/";//10.28.83.150:3000/
     public API={
         loginApi: this.httpHader + this.address+"post/login",
         postApi: this.httpHader + this.address+"get/post",
